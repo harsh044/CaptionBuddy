195d818d8bac0fef523c3bd96b3da75f5ceb9ed3
Harshad <72267094+harsh044@users.noreply.github.com>
2024-12-21 14:59:57 +0530

    add middleware

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from pathlib import Path
 from fastapi import FastAPI, APIRouter
 from app.api import api_router
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
 
 BASE_PATH = Path(__file__).resolve().parent
 root_router = APIRouter()
 app = FastAPI(title="Caption Generator API's")
+
+class CSPMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers['Content-Security-Policy'] = "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+        return response
+
+app.add_middleware(CSPMiddleware)
 
 app.include_router(api_router)
 
