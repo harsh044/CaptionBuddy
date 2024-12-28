f35ade472444c902918f656052f4613c43888a74
Harshad <72267094+harsh044@users.noreply.github.com>
2024-12-28 19:28:21 +0530

    update origin

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.include_router(api_router)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # React app's origins
+    allow_origins=["*"],  # React app's origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
