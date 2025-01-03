f6c9efd1f0ca54fa0481af0b0f9386bee1b89d34
Harshad <72267094+harsh044@users.noreply.github.com>
2025-01-03 12:23:59 +0530

    ref: update code and dokerize

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-if __name__ == "__main__":
-    import uvicorn
-    # Use this for debugging purposes only       
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    # uvicorn.main()
+# if __name__ == "__main__":
+#     import uvicorn
+#     # Use this for debugging purposes only       
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+#     # uvicorn.main()
